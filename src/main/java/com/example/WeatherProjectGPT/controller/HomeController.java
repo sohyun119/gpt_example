420efed1d0b7749d75d0fbc441e73c25056b3c19
Sohyun <sh-119@naver.com>
2023-04-25 20:25:21 +0900

    Char GPT API 테스트중

--- src/main/java/com/example/WeatherProjectGPT/controller/HomeController.java
+++ src/main/java/com/example/WeatherProjectGPT/controller/HomeController.java
@@ -1,8 +1,14 @@
 package com.example.WeatherProjectGPT.controller;
 
 import com.example.WeatherProjectGPT.service.HomeService;
+import com.theokanning.openai.OpenAiService;
+import com.theokanning.openai.completion.CompletionRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class HomeController {
@@ -13,10 +19,27 @@ public class HomeController {
 
     public final HomeService homeService;
 
+    @Value("${openAiApi.key}")
+    public String openAiToken;
+
     @GetMapping("/home")
     public String homeTest(){
         return "homeView";
     }
+
+    @GetMapping("/chatGPT")
+    public ResponseEntity<?> chatGPT(){
+
+        OpenAiService service = new OpenAiService(openAiToken);
+        CompletionRequest completionRequest = CompletionRequest.builder()
+                .prompt("how are you")
+                .model("text-davinci-003")
+                .echo(true)
+                .build();
+        return ResponseEntity.ok(service.createCompletion(completionRequest).getChoices());
+    }
+
+    
 
 
 }
